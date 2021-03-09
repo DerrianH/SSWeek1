6b506a5fbabca0598e0aa3f375332b12fcfbd411
Derrian <dsh0018au@gmail.com>
2021-03-09 01:00:27 -0600

    Day One

--- Day1/src/com/ss/training/dayone/StarGen.java
+++ Day1/src/com/ss/training/dayone/StarGen.java
@@ -4,6 +4,7 @@
 package com.ss.training.dayone;
 
 /**
+ * Prints a triangle with *
  * @author derrianharris
  *
  */
@@ -26,23 +27,56 @@ public class StarGen {
 		}
 		
 		
-		for(int i = 0; i <= 4; i++) {
+		for(int i = 0; i < 4; i++) {
 			int j = i * 2 + 1;
-			System.out.println(genCharString(j,'*'));
+			System.out.println(genCharStringCentered(11,j,'*'));
 		}
 		System.out.println(genCharString(11,'-'));
 		
-		for(int i = 0; i <= 4; i++) {
-			System.out.println(genCharString(i,'*'));
+		for(int i = 3; i >= 0; i--) {
+			int j = i * 2 + 1;
+			System.out.println(genCharStringCentered(11,j,'*'));
 		}
-		System.out.println(genCharString(9,'-'));
+		System.out.println(genCharString(12,'-'));
+		
+
 	}
 	
+	
+	/**
+	 * Generates a number of chars in a string
+	 * @param num
+	 * @param symb
+	 * @return
+	 */
 	public static String genCharString(int num, char symb) {
 		String result = "";
 		
 		for(int i = 0; i < num; i++) {
 			result += symb;
+		}
+		return result;
+	}
+
+
+	/**
+	 * Generates a number of chars in a string centered around a middle point
+	 * @param lineLength
+	 * @param num
+	 * @param symb
+	 * @return
+	 */
+	public static String genCharStringCentered(int lineLength,int num, char symb) {
+		String result = "";
+		int halfNum = (int) Math.ceil((lineLength - num) * 0.5);
+		for(int i = 0; i < lineLength; i++) {
+			
+			if(i >= halfNum && i < lineLength - halfNum ) {
+				result += symb;
+				continue;
+			}
+			result += " ";
+			
 		}
 		return result;
 	}
