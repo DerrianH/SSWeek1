2956e15e4aa5c720dceb0606531c97094e3c6bfa
derrianharris <derrianharris@localhost>
2021-03-08 21:06:57 -0600

    Day 1

---
+++ Day1/src/com/ss/training/dayone/RandomNumApp.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.ss.training.dayone;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author derrianharris
+ *
+ */
+public class RandomNumApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); // Create a Scanner object
+	    Random rand = new Random();
+	    Integer upperbound = 100;
+	    Integer randNum = rand.nextInt(upperbound + 1);
+	    Integer guesses = 5;
+
+	    while (guesses > 0) {
+	      System.out.println(
+	        "Pick a number between 1 - 100. You have " +
+	        guesses +
+	        (guesses == 1 ? " guess " : " guesses ") + "left"
+	      );
+	      Integer userInput = 0;
+	      try {
+	    	  userInput = sc.nextInt();
+	      }catch(Exception e) {
+	    	  System.out.println("Invalid guess.");
+	    	  sc.nextLine();
+	    	  continue;
+	      };
+	      
+	      if (userInput >= 0 && userInput < upperbound) {
+	  	        if (Math.abs(userInput - randNum) <= 10) {
+	  	          System.out.println(
+	  	            "Nice Job! Input: " + userInput + " Answer: " + randNum
+	  	          );
+	  	          break;
+	  	        } else {
+	  	        	if(guesses > 1) {
+	  	        		System.out.println("Try again.");
+	  	        	}
+	  	          guesses--;
+	  	        }
+	  	      } else {
+	  	        System.out.println("Input out of range");
+	  	      }
+	  	    
+	  	    if(guesses <= 0) {
+	  	    	System.out.println("Sorry out of guesses... Answer: " + randNum);
+	  	    }
+	      
+
+	}
+
+	}
+}
---
+++ Day1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author derrianharris
+ *
+ */
+module Day1 {
+}
